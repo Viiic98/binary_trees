10efe8d0b748774d1a307d2d06d84f1b5f0905cd
viiic98 <xathovic@gmail.com>
2020-03-11 18:33:54 +0000

    Add task that insert to right

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -2,27 +2,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
-*
-*
-*/
+ * binary_tree_insert_right - Insert a new node to right
+ * @parent: parent of the new tree
+ * @value: value of the new tree
+ * Return: pointer to new tree
+ */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-    binary_tree_t *node;
+	binary_tree_t *node;
 
-    if (parent == NULL)
-        return (NULL);
-    node = malloc(sizeof(binary_tree_t));
-    if (parent->right != NULL)
-    {
-        node->right = parent->right;
-        parent->right->parent = node;
-    }
-    else
-        node->right = NULL;
-    parent->right = node;
-    node->parent = parent;
-    node->left = NULL;
-    node->n = value;
+	if (parent == NULL)
+		return (NULL);
+	node = malloc(sizeof(binary_tree_t));
+	if (parent->right != NULL)
+	{
+		node->right = parent->right;
+		parent->right->parent = node;
+	}
+	else
+		node->right = NULL;
+	parent->right = node;
+	node->parent = parent;
+	node->left = NULL;
+	node->n = value;
 
-    return (node);
+	return (node);
 }
